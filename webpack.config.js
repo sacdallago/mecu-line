d3a83fdf639ce0d40e47fa41f00fe41d68cf277d
Christian Dallago <code@dallago.us>
2017-04-25 16:56:48 +0200

    started implementing average

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
-    entry: __dirname + '/lib/index.js',
+    entry: ['babel-polyfill', path.resolve(__dirname + '/lib/index.js')],
     output: {
-         path: __dirname + '/build',
-         filename: 'mecu-line.js',
-         libraryTarget: 'var',
-         library: 'MecuLine'
-    }
- };
+        path: __dirname + '/build',
+        filename: 'mecu-line.js',
+        libraryTarget: 'var',
+        library: 'MecuLine'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ["es2016"]
+                }
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader",
+                query: {
+                    mimetype: "image/png"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ]
+};
